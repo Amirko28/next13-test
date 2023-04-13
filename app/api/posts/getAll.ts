07c786a9fe979b1f1ccfbabf997cb5f87a89a278
Amirko28 <amirko28@gmail.com>
2023-04-13 13:54:35 +0000

    Update 6 files

--- app/api/posts/getAll.ts
+++ app/api/posts/getAll.ts
@@ -1,9 +1,8 @@
-import { db } from '@/api/db';
 import { NextResponse } from 'next/server';
 
 export const revalidate = 10;
 
 export const getAllPosts = async () => {
-    const data = await db.post.findMany();
+    const data = [{id: 1, text: 'hello'}, {id: 2, text: 'world'}]
     return NextResponse.json({ data });
 };
